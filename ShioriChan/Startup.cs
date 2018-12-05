2930f3e6a59019d8a32e8b5ef89da676f42d062f
Kawachi <kawachi.shusaku@opensesame-tech.com>
2018-12-05 13:11:31 +0900

    Initial Asp.Net Core commit

---
+++ ShioriChan/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShioriChan {
+
+	public class Startup {
+
+		public IConfiguration Configuration { get; }
+
+		public Startup( IConfiguration configuration ) => this.Configuration = configuration;
+		
+		/// <summary>
+		/// This method gets called by the runtime. Use this method to add services to the container.
+		/// </summary>
+		public void ConfigureServices( IServiceCollection services ) => services.AddMvc().SetCompatibilityVersion( CompatibilityVersion.Version_2_1 );
+		
+		/// <summary>
+		/// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// </summary>
+		public void Configure( IApplicationBuilder app , IHostingEnvironment env ) {
+			if( env.IsDevelopment() ) {
+				app.UseDeveloperExceptionPage();
+			}
+			else {
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseMvc();
+		}
+
+	}
+
+}
