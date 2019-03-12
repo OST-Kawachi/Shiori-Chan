350f2425d406c69923e34a6525f27d24591cf209
aiyama1 <aiyama.katsunori@opensesame-tech.com>
2019-03-12 20:08:37 +0900

    scheduleクラス作成

---
+++ ShioriChan/Services/Features/Schedule/IScheduleservice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShioriChan.Services.Features.Schedule
+{
+
+
+        /// <summary>
+        /// 部屋情報
+        /// </summary>
+        public interface IScheduleservice
+        {
+
+            /// <summary>
+            /// //5分前通知
+            /// </summary>
+            /// <param name="parameter">パラメータ</param>
+            Task Notification(JToken parameter);
+
+
+
+        }
+}
---
+++ ShioriChan/Services/Features/Schedule/Scheduleservice.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShioriChan.Services.Features.Schedule
+{
+    class Scheduleservice
+    {
+    }
+}
