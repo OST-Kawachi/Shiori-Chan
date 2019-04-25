d9960889d7e94a5138761d104fd5a003f814ff31
nakashima <nakashima.mayuko@opensesame-tech.com>
2019-04-25 14:08:11 +0900

    5分前に表示（完？）

--- ShioriChan/Repositories/Schedules/IScheduleRepository.cs
+++ ShioriChan/Repositories/Schedules/IScheduleRepository.cs
@@ -1,9 +1,28 @@
-﻿namespace ShioriChan.Repositories.Schedules {
+﻿using ShioriChan.Entities;
+using System.Collections.Generic;
+
+namespace ShioriChan.Repositories.Schedules {
 
 	/// <summary>
 	/// スケジュールRepository
 	/// </summary>
 	public interface IScheduleRepository {
-	}
+        /// <summary>
+        /// 通知する予定の名称を取得
+        /// </summary>
+        /// <return>メッセージ</return>
+        Schedule GetSchedule();
 
+        /// <summary>
+        /// 全員のユーザIDを取得
+        /// </summary>
+        /// <returns>ユーザID</returns>
+        List<string> GetAllUserId();
+
+        /// <summary>
+        /// 通知済みであることを更新する
+        /// </summary>
+        /// <param name="eventSeq">通知したスケジュールのイベント管理番号</param>
+        void UpdateNotified(int eventSeq);
+    }
 }
--- ShioriChan/Repositories/Schedules/ScheduleRepository.cs
+++ ShioriChan/Repositories/Schedules/ScheduleRepository.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using ShioriChan.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ShioriChan.Repositories.Schedules {
 
@@ -30,6 +34,61 @@ namespace ShioriChan.Repositories.Schedules {
 			this.model = model;
 		}
 
-	}
+        /// <summary>
+        /// 通知する予定の名称を取得
+        /// </summary>
+        /// <return>メッセージ</return>
+        public Schedule GetSchedule()
+        {
+            Schedule schedule = null;
+
+            this.logger.LogTrace("Start");
+            DateTime now = DateTime.Now;
+            schedule = this.model.Schedules
+                 .Where(s => now + TimeSpan.FromMinutes(5) > s.StartDatetime)
+                 .Where(s => s.IsNotified == false)
+                 .FirstOrDefault();
+
+            this.logger.LogTrace("End");
+
+            return schedule;
+        }
+        
+        /// <summary>
+        /// 全員のユーザIDを取得
+        /// </summary>
+        /// <returns>ユーザID</returns>
+        public List<string> GetAllUserId()
+        {
+            this.logger.LogTrace("Start");
+            List<string> userIds = this.model.UserInfos
+               .Where(u => !string.IsNullOrEmpty(u.Id))
+               .Select(u => u.Id)
+               .ToList();
+
+            this.logger.LogTrace("End");
+            return userIds;
+        }
+
+        /// <summary>
+        /// 通知済みであることを更新する
+        /// </summary>
+        /// <param name="eventSeq">通知したスケジュールのイベント管理番号</param>
+        /// <return>メッセージ</return>
+        public void UpdateNotified(int eventSeq)
+        {
+            Schedule updateSchedule = null;
+
+            this.logger.LogTrace("Start");
+            updateSchedule = this.model.Schedules
+                 .Where(s => s.EventSeq == eventSeq)
+                 .FirstOrDefault();
+
+            updateSchedule.IsNotified = true;
+            this.model.SaveChanges();
+
+            this.logger.LogTrace("End");
+        }
+    }
 
 }
